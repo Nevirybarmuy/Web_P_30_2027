757c17c386d6d63da7e0567461b070a5c071871a
barsa <barsa@TEMP>
2026-02-09 02:29:24 +0300

    Проверка свзяи

--- WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c23e17409219b057e7875cb48f73ee7bff4aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0477d99ed12646acec246d82cbb2c491882e9770")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
